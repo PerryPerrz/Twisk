9f92fb77e9ac76cef450082b45929e07be05537e
YVOZ Ludovic <ludovic.yvoz1@etu.univ-lorraine.fr>
2021-02-02 15:09:37 +0100

    Ajout du test de l'itérateur de la classe Etape

--- src/twisk/tests/EtapeTest.java
+++ src/twisk/tests/EtapeTest.java
@@ -69,4 +69,25 @@ class EtapeTest {
         assertTrue(gui1.estUnGuichet());
         assertTrue(gui2.estUnGuichet());
     }
+
+    @Test
+    void iterator() {
+        //On teste si l'itérateur parcours bien tous les successeurs d'une étape et si les étapes parcourues sont les bonnes
+
+        act1.ajouterSuccesseur(act2, act3);
+        int count = 0;
+        for (Etape etape : act1) {
+            assertTrue(etape.estUneActivite());
+            count++;
+        }
+        assertEquals(count, act1.nbSuccesseurs());
+
+        count = 0;
+        act2.ajouterSuccesseur(gui1, gui2, gui3);
+        for (Etape etape : act2) {
+            assertTrue(etape.estUnGuichet());
+            count++;
+        }
+        assertEquals(count, act2.nbSuccesseurs());
+    }
 }
