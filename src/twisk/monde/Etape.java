e83892948cc24720b99e1fb35cfc23619794b650
YVOZ Ludovic <ludovic.yvoz1@etu.univ-lorraine.fr>
2021-02-02 11:34:52 +0100

    Ajout de la collection (ArrayList) d'étapes successeurs

--- src/twisk/monde/Etape.java
+++ src/twisk/monde/Etape.java
@@ -1,17 +1,20 @@
 package twisk.monde;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 
 public abstract class Etape implements Iterable<Etape> {
-
+    protected ArrayList<Etape> succ;
     protected String nom;
 
     public Etape(String nom) {
         this.nom = nom;
+        succ = new ArrayList<>(5);  //On mets 5 en capacité initiale pour l'instant (à ajuster par la suite)
     }
 
     public void ajouterSuccesseur(Etape... e) {
-
+        succ.addAll(Arrays.asList(e));
     }
 
     public boolean estUneActivite() {
