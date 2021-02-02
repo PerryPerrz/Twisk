9b5b21ec2ca163bcbbc83ae2168ecc7ff9d8917b
iopeti2u <hugo.iopeti5@etu.univ-lorraine.fr>
2021-02-02 11:48:54 +0100

    tests

---
+++ src/twisk/tests/EtapeTest.java
@@ -0,0 +1,22 @@
+package twisk.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EtapeTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void ajouterSuccesseur() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void estUneActivite() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void estUnGuichet() {
+    }
+}
---
+++ src/twisk/tests/GuichetTest.java
@@ -0,0 +1,17 @@
+package twisk.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuichetTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void estUnGuichet() {
+    }
+}
