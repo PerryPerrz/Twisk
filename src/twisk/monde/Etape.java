d62fe472554e3a66152d84a79c76e84cf07fa506
YVOZ Ludovic <ludovic.yvoz1@etu.univ-lorraine.fr>
2021-02-02 11:17:38 +0100

    Code de la plupart des fonctions basiques de la classe Etape

--- src/twisk/monde/Etape.java
+++ src/twisk/monde/Etape.java
@@ -1,4 +1,28 @@
 package twisk.monde;
 
-public abstract class Etape {
+import java.util.Iterator;
+
+public abstract class Etape implements Iterable<Etape> {
+
+    protected String nom;
+
+    public Etape(String nom) {
+        this.nom = nom;
+    }
+
+    public void ajouterSuccesseur(Etape... e) {
+
+    }
+
+    public boolean estUneActivite() {
+        return false;
+    }
+
+    public boolean estUnGuichet() {
+        return false;
+    }
+
+    public Iterator<Etape> iterator() {
+        return null;
+    }
 }
