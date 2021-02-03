3a2d15d92cc330d8a9bc145ffa888dd03e2058c1
YVOZ Ludovic <ludovic.yvoz1@etu.univ-lorraine.fr>
2021-02-03 14:45:48 +0100

    Hiérarchisation des tests

---
+++ src/twisk/tests/ActiviteRestreinteTest.java
@@ -0,0 +1,8 @@
+package twisk.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActiviteRestreinteTest extends ActiviteTest {
+    //La classe ActiviteRestreinteTest hérite des tests de la classe ActiviteTest ce qui permet de ne pas avoir à réecrire ces tests
+    //Nous écrirons dans cette classe tous les tests qui sont propres à la classe ActiviteeRestreinte une fois que nous aurons de telles fonctions à tester
+}
---
+++ src/twisk/tests/SasSortieTest.java
@@ -0,0 +1,8 @@
+package twisk.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SasSortieTest extends ActiviteTest {
+    //La classe SasSortieTest hérite des tests de la classe ActiviteTest ce qui permet de ne pas avoir à réecrire ces tests
+    //Nous écrirons dans cette classe tous les tests qui sont propres à la classe SasSortie une fois que nous aurons de telles fonctions à tester
+}
