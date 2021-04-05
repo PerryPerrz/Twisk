234d948a57ea3f726088a4b3a49a937cb2971d34
iopeti2u <hugo.iopeti5@etu.univ-lorraine.fr>
2021-04-05 14:58:50 +0200

    Corrections des tests.

--- src/twisk/tests/SasEntreeTest.java
+++ src/twisk/tests/SasEntreeTest.java
@@ -18,9 +18,9 @@ class SasEntreeTest extends ActiviteTest {
     @Test
     void toC() {//Pour ce test, on travaille sur des mondes non-cohérents donc on doit rajouter des sas de sorties en tant que successeurs des sas d'entrées à tester
         sasE1.ajouterSuccesseur(sasS1);
-        assertEquals(sasE1.toC(), "entrer(69);\ndelai(5, 2);\ntransfert(69, 72);\n");
+        assertEquals(sasE1.toC(), "entrer(69);\ndelai(4, 2);\ntransfert(69, 72);\n");
         sasE2.ajouterSuccesseur(act1);
         act1.ajouterSuccesseur(sasS2);
-        assertEquals(sasE2.toC(), "entrer(70);\ndelai(5, 2);\ntransfert(70, 60);\ndelai(5, 2);\ntransfert(60, 73);\n");
+        assertEquals(sasE2.toC(), "entrer(70);\ndelai(4, 2);\ntransfert(70, 60);\ndelai(4, 2);\ntransfert(60, 73);\n");
     }
 }
