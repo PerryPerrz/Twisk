504c635bbd330f47de117bfacaab6b5b80d56394
YVOZ Ludovic <ludovic.yvoz1@etu.univ-lorraine.fr>
2021-04-15 12:09:09 +0200

    Fin question 2  Partie B  TP 8

--- src/twisk/ClientTwisk.java
+++ src/twisk/ClientTwisk.java
@@ -4,7 +4,11 @@ import twisk.monde.Activite;
 import twisk.monde.ActiviteRestreinte;
 import twisk.monde.Guichet;
 import twisk.monde.Monde;
-import twisk.simulation.Simulation;
+import twisk.outils.ClassLoaderPerso;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * La classe ClientTwisk.
@@ -35,23 +39,23 @@ public class ClientTwisk {
         return monde;
     }
 
-    /*A décommenter lorsque l'implémentation des multiples successeurs à une même Activité est codée
+
     public Monde ConstruMonde2() {
         Monde monde = new Monde();
         Activite balade = new Activite("balade au zoo");
         Activite parking = new ActiviteRestreinte("parking");
         Guichet guichet2 = new Guichet("guichet2", 2);
-        Guichet guichet2500 = new Guichet("guichet2500", 2500);
+        Guichet guichet3 = new Guichet("guichet3", 3);
         Activite toboggan = new ActiviteRestreinte("toboggan");
-        guichet2500.ajouterSuccesseur(parking);
+        guichet3.ajouterSuccesseur(parking);
         parking.ajouterSuccesseur(balade);
         balade.ajouterSuccesseur(guichet2);
         guichet2.ajouterSuccesseur(toboggan);
-        monde.aCommeEntree(guichet2500, balade);
+        monde.aCommeEntree(guichet3);
         monde.aCommeSortie(toboggan);
-        monde.ajouter(guichet2500, parking, balade, guichet2, toboggan);
+        monde.ajouter(guichet3, parking, balade, guichet2, toboggan);
         return monde;
-    }*/
+    }
 
     /**
      * Le point d'entrée de l'application.
@@ -64,16 +68,30 @@ public class ClientTwisk {
         ClientTwisk clientTwisk = new ClientTwisk();
         monde = clientTwisk.ConstruMonde1();
 
-        //Début de la simulation
-        Simulation simulation = new Simulation();
-        simulation.setNbClients(5);
-        simulation.simuler(monde);
+        //Début de la simulation du premier monde
+        ClassLoaderPerso classLoaderPerso = new ClassLoaderPerso(ClientTwisk.class.getClassLoader());
+        try {
+            Class<?> clS = classLoaderPerso.loadClass("twisk.simulation.Simulation");
+            Constructor<?> constructeur = clS.getDeclaredConstructor();
+            Object instanceSim = constructeur.newInstance();
+            Method fonctionSetNbClients = clS.getMethod("setNbClients", int.class);
+            fonctionSetNbClients.invoke(instanceSim, 5);
+            Method fonctionSimuler = clS.getMethod("simuler", Monde.class);
+            fonctionSimuler.invoke(instanceSim, monde);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Erreur lors du chargement de la classe twisk.simulation.Simulation !");
+        } catch (NoSuchMethodException e) {
+            System.out.println("Erreur, fonction non trouvée dans la classe twisk.simulation.Simulation !");
+        } catch (InvocationTargetException e) {
+            System.out.println("Erreur lors de l'appel d'une fonction de la classe twisk.simulation.Simulation !");
+        } catch (IllegalAccessException e) {
+            System.out.println("Erreur, appel illegal d'une fonction de la classe twisk.simulation.Simulation !");
+        } catch (InstantiationException e) {
+            System.out.println("Erreur lors de l'instanciation de la classe twisk.simulation.Simulation !");
+        }
 
-        /* A décommenter lorsque l'implémentation des multiples successeurs à une même Activité est codée
         //Simulation n°2
         monde = clientTwisk.ConstruMonde2();
-        simulation.setNbClients(5);
-        simulation.simuler(monde);
-        */
+        
     }
 }
